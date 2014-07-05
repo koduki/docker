4b26f35b8162cd94aa3607db82110bb09dcf2236
koduki <pascal-m3@gmail.com>
2014-07-05 10:17:10 +0000

    modify for base and app

--- images/app/run.sh
+++ images/app/run.sh
@@ -5,4 +5,6 @@ IMAGE="koduki/app"
 MOUNT_PROGRAM=" -v /home/core/program:/home/docker/program "
 MOUNT_MYSQL=" -v /home/core/host_lib/mysql:/var/lib/mysql "
 
+alias ssh-docker=../../scripts/ssh-docker.sh
+
 docker run -d -P $MOUNT_PROGRAM $MOUNT_MYSQL --name $NAME --hostname $NAME $IMAGE
